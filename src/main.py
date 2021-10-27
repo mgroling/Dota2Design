97db25bdf3b74b38d1c93592381eeb0df358503e
marc131183 <marc.groeling@gmail.com>
2021-10-27 17:20:09 +0200

    ...

--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ from functions_summarize import *
 from functions_photoshop import *
 
 if __name__ == "__main__":
-    pics, texts = getAfterGameStatistics(6227492909)
+    # pics, texts = getAfterGameStatistics(6227305557)
+
+    # doc = openSession(os.getcwd() + "/After_Game_Statistics_25_10_2021.psd")
+
+    # for key, value in pics.items():
+    #     changeImage(doc, key, value)
+
+    # for key, value in texts.items():
+    #     changeText(doc, key, value)
 
     doc = openSession(os.getcwd() + "/After_Game_Statistics_25_10_2021.psd")
 
-    for key, value in pics.items():
-        changeImage(doc, key, value)
-
-    for key, value in texts.items():
-        changeText(doc, key, value)
+    # doc.artLayers["radiant_networth"].textItem.position = [300, 300]
+    print(doc.artLayers["radiant_networth"].textItem)
