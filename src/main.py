ba253b74af2c654bf9090b4ee4ce169975878223
marc131183 <marc.groeling@gmail.com>
2021-12-01 18:42:31 +0100

    small cleanup

--- src/main.py
+++ src/main.py
@@ -1,19 +1,13 @@
 from functions_summarize import *
-
-# from functions_photoshop import *
+from functions_photoshop import *
 
 if __name__ == "__main__":
-    # pics, texts = getAfterGameStatistics(6227305557)
-
-    # doc = openSession(os.getcwd() + "/After_Game_Statistics_25_10_2021.psd")
-
-    # for key, value in pics.items():
-    #     changeImage(doc, key, value)
-
-    # for key, value in texts.items():
-    #     changeText(doc, key, value)
+    pics, texts = getAfterGameStatistics(6227305557)
 
     doc = openSession(os.getcwd() + "/After_Game_Statistics_25_10_2021.psd")
 
-    # doc.artLayers["radiant_networth"].textItem.position = [300, 300]
-    print(doc.artLayers["radiant_networth"].textItem)
+    for key, value in pics.items():
+        changeImage(doc, key, value)
+
+    for key, value in texts.items():
+        changeText(doc, key, value)
